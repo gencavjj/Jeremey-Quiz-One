f907fd38b74b8ddbe93c9e204f3e5a9b1345c9b7
Dennis Kalaygian <dennis.kalaygian@Trinity.Transport>
2018-10-23 13:09:47 -0400

    fixed basic

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,7 +3,12 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        
+        String sampleMessage = createSampleMessage();
+        System.out.println(sampleMessage);
+    }
+
+    private static String createSampleMessage() {
+        return "This is a sample message";
     }
 
 }
