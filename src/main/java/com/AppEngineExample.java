1846d9ef3e758d65a2b2a5cf4c07539e099e9aad
Jeremy Gencavage <Jeremy@Jeremys-MacBook-Air.local>
2018-11-04 10:37:30 -0500

    Updating switch statements

--- src/main/java/com/AppEngineExample.java
+++ src/main/java/com/AppEngineExample.java
@@ -35,28 +35,43 @@ public class AppEngineExample {
         System.out.println("3) Exit");
 
         String userResponseToMenuPrompt = inputScanner.nextLine();
-
         switch (userResponseToMenuPrompt) {
             case ("1"):
 
-                while (appIsRunning) {
+                int numberOfCharacters;
+                int numberOfPhrases;
 
+                System.out.println("How many characters would you like to add? ");
+                numberOfCharacters = inputScanner.nextInt();
+                inputScanner.nextLine();
+
+                for (int i = 0; i < numberOfCharacters; i++) {
                     System.out.println("Enter a character name: ");
-                    characters.add(inputScanner.next());
-                    System.out.println(characters);
-                    System.out.println("Would you like to create another character? ");
-                    if (userResponseToMenuPrompt.contentEquals("yes"))
-                        appIsRunning = true;
-                    else
+                    characters.add(i, inputScanner.nextLine());
 
-                        appIsRunning = false;
+                    System.out.println("How many phrases would you like to add? ");
+                    numberOfPhrases = inputScanner.nextInt();
+                    inputScanner.nextLine();
+
+                    for (int x = 0; x < numberOfPhrases; x++) {
+                        System.out.println("Enter phrases for individual: ");
+                        catchPhrases.put("Phrase 1 ", inputScanner.nextLine());
+
+                    }
+
                 }
+                System.out.println(characters);
+                System.out.println(catchPhrases);
+
                 // Create character and add to list
                 // Accept user input for character name (string)
                 // Scanner input
                 // Accept user input for catch phrases and add to hash map for character (Map<CharacterName, CatchPhrase>)
                 break;
             case ("2"):
+
+                System.out.println("Here are the list of Characters " + characters + "Please select one to play with. ");
+
                 // Display list of characters and allow user to select from list to play catch phrases
                 // Loop through hash map of catch phrases and print each one for selected character
                 break;
