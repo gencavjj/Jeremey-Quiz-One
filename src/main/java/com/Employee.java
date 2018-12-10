b92d3d98b73024aa2cdd2dd69f3bf2dfdde1b5ce
Dennis Kalaygian <dennis.kalaygian@Trinity.Transport>
2018-12-09 20:15:48 -0500

    Started employee management

---
+++ src/main/java/com/Employee.java
@@ -0,0 +1,39 @@
+package com;
+
+public class Employee {
+
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+
+    public Employee() {}
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
